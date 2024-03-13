80cf192a5eacfa5296b618dc7b6e2cb89135b7df
sapje1 <evansj31@cardiff.ac.uk>
2024-03-13 16:32:56 +0000

    working version of quickqc analysis, with auto email

--- autoqc_email.bash
+++ autoqc_email.bash
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# FMRIQC
 # explicit definition of png files.  use imagemagick's convert to merge the pngs into a single file 
 convert -append /cubric/collab/108_QA/QA3TM/fmriqc_glover/summary/3TM_fmriqc_latest.png /cubric/collab/108_QA/QA7T/fmriqc_glover/summary/7T_fmriqc_latest.png /cubric/collab/108_QA/QA3TE/fmriqc_glover/summary/3TE_fmriqc_latest.png  /cubric/collab/108_QA/QA3TW/fmriqc_glover/summary/3TW_fmriqc_latest.png  /cubric/collab/108_QA/fmriqc_latest.png
-
 /home/sapje1/code/python_mrobjects/qc/autoqc_status.py | mailx -r mriqc@cardiff.ac.uk -s "fmriqc (Glover)" -a /cubric/collab/108_QA/fmriqc_latest.png evansj31@cardiff.ac.uk
 
+# QUICKQC
+convert -append /cubric/collab/108_QA/QA3TM/quick_SNR_gre3D/summary/3TM_quickqc_latest.png /cubric/collab/108_QA/QA7T/quick_SNR_gre3D/summary/7T_quickqc_latest.png /cubric/collab/108_QA/QA3TE/quick_SNR_gre3D/summary/3TE_quickqc_latest.png  /cubric/collab/108_QA/QA3TW/quick_SNR_gre3D/summary/3TW_quickqc_latest.png  /cubric/collab/108_QA/quickqc_latest.png
+/home/sapje1/code/python_mrobjects/qc/autoqc_status.py | mailx -r mriqc@cardiff.ac.uk -s "mriqc (QuickQc)" -a /cubric/collab/108_QA/quickqc_latest.png evansj31@cardiff.ac.uk
 
 spikefiles=`find /cubric/collab/108_QA -ipath '*spike_stats.png' -ctime -1 | xargs`
 
